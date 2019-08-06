be8c84d68afbdd5ded55cee3ba93a586fee320d1
Pető Zoltán <bajgli@gmail.com>
2019-08-06 14:10:50 +0200

    documentation added

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # react-typescript-demo
 This is a demo app proving how to load 10K contacts into a select box
+
+# Node version
+
+Node.js: Minimum 10.16.1 - For upgrade refer to https://nodejs.org/en/
+
+# Task description
+1. Code a small node.js app with the frontend framework of your choice, it should load a list of 10k contacts “name, phone number, email” from a json file into a html dropdown component asynchronously
+2. Unit test with a test framework of your choice
+3. Describe some performance enhancement alternatives if you had more time to complete the task
+
+# How to run the app
+1. Clone
+2. cd contacts-generator && npm install && node index.js // generating contacts.json file
+3. cd ../frontend && npm install && npm start
+ 
+# Performance enhancenments
+As the rendering depends on *react-virtualized-select* package capabilities there is not much to do with loading/rendering performance. Tested it on my computer and it is lightning fast.
+
+# Demo
+Demo can be viewed here: https://www.blockspire.com/bequant-coding-test/
--- contacts-generator/index.js
+++ contacts-generator/index.js
@@ -1,24 +1,21 @@
-const fs = require('fs');
-const fsPromises = fs.promises;
-const faker = require('faker');
-
-const GENERATE_AMOUNT = 10000;
-
-const contacts = [];
+const fs = require('fs')
+const faker = require('faker')
+const GENERATE_AMOUNT = 10000
+let contacts = []
 
 for(let x = 0; x < GENERATE_AMOUNT; x++) {
-  const contact = {
-    name: faker.name.findName(),
-    phoneNumber: faker.phone.phoneNumber(),
-    email: faker.internet.email()
-  };
-
-  contacts.push(contact);
+    const contact = {
+        name: faker.name.findName(),
+        phoneNumber: faker.phone.phoneNumber(),
+        email: faker.internet.email()
+    }
+    contacts.push(contact)
 }
 
-fsPromises.writeFile(`${__dirname}/../frontend/public/contacts.json`, JSON.stringify(contacts))
-.then(() => {
-  console.log('Wrote Contacts!');
-}).catch((err) => {
-  console.log('Error occurred: ', err.message);
-});
+fs.writeFile('../frontend/public/contacts.json', JSON.stringify(contacts), 'utf-8', function(err){
+    if(err) {
+        console.log(err.message)
+    } else {
+        console.log('Contacts write was successful')
+    }
+})
